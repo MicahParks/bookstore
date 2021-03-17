d36f8c584d7f601b1880af541b6e5274f0d5b80b
Micah Parks <git@micahparks.com>
2021-03-16 20:31:13 -0400

    Starting add book and alerts.

--- frontend/alert.js
+++ frontend/alert.js
@@ -1,2 +1,7 @@
+// <div class="alert alert-warning alert-dismissible fade show" role="alert">
+//   <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+//   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+// </div>
 
-new bootstrap.Alert()
+
+// https://www.w3docs.com/snippets/javascript/how-to-create-a-new-dom-element-from-html-string.html
